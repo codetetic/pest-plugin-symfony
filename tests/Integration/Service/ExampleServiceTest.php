63d457be95ed88dcaf9c246ac5089606c6d87cfa
Peter Measham <github@codetetic.co.uk>
2025-05-20 18:31:08 +0100

    improve test

--- tests/Integration/Service/ExampleServiceTest.php
+++ tests/Integration/Service/ExampleServiceTest.php
@@ -1,14 +1,10 @@
 <?php
 
-use function Pest\Symfony\bootKernel;
 use function Pest\Symfony\getContainer;
 
 it('can get and use service', function () {
-    bootKernel();
-
-    $container = getContainer();
-
-    $service = $container->get(App\Service\ExampleService::class);
+    /** @var App\Service\ExampleService */
+    $service = getContainer()->get(App\Service\ExampleService::class);
 
     expect($service->string())->toBe('string');
 });
