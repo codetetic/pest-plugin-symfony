efe09531095c348cfd3296b060f7e77977080ec6
Peter Measham <github@codetetic.co.uk>
2025-05-21 21:14:25 +0100

    fixes from rector

--- tests/Application/WebTest.php
+++ tests/Application/WebTest.php
@@ -6,29 +6,29 @@ use function Pest\Symfony\Web\getCrawler;
 use function Pest\Symfony\Web\getRequest;
 use function Pest\Symfony\Web\getResponse;
 
-it('can create a client', function () {
+it('can create a client', function (): void {
     expect(createClient())->toBeInstanceOf(Symfony\Bundle\FrameworkBundle\KernelBrowser::class);
 });
 
-it('can get a client', function () {
+it('can get a client', function (): void {
     createClient();
 
     expect(getClient())->toBeInstanceOf(Symfony\Bundle\FrameworkBundle\KernelBrowser::class);
 });
 
-it('can get crawler', function () {
+it('can get crawler', function (): void {
     createClient()->request('GET', '/example');
 
     expect(getCrawler())->toBeInstanceOf(Symfony\Component\DomCrawler\Crawler::class);
 });
 
-it('can get request', function () {
+it('can get request', function (): void {
     createClient()->request('GET', '/example');
 
     expect(getRequest())->toBeInstanceOf(Symfony\Component\HttpFoundation\Request::class);
 });
 
-it('can get response', function () {
+it('can get response', function (): void {
     createClient()->request('GET', '/example');
 
     expect(getResponse())->toBeInstanceOf(Symfony\Component\HttpFoundation\Response::class);
