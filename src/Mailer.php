6dedc5b7d4acce1d7b8242faf9e959d399c8c859
Peter Measham <github@codetetic.co.uk>
2025-05-22 09:00:01 +0100

    improve test assert running

--- src/Mailer.php
+++ src/Mailer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Pest\Symfony\Mailer;
 
 use Pest\Expectation;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Mailer\Event\MessageEvent;
 use Symfony\Component\Mime\RawMessage;
 
@@ -36,43 +37,49 @@ function getMailerMessage(int $index = 0, ?string $transport = null): ?RawMessag
 
 function extend(Expectation $expect): void
 {
-    $expect->extend('toBeEmailCount', function (int $count, ?string $transport = null): void {
-        $this->assertEmailCount($count, $transport);
+    function runner(WebTestCase $test, string $method, array $arguments = []): WebTestCase
+    {
+        call_user_func([$test, $method], ...$arguments);
+        return $test;
+    }
+
+    $expect->extend('toBeEmailCount', function (int $count, ?string $transport = null): WebTestCase {
+        return runner(test(), 'assertEmailCount', [$count, $transport]);
     });
 
-    $expect->extend('toBeQueuedEmailCount', function (int $count, ?string $transport = null): void {
-        $this->assertQueuedEmailCount($count, $transport);
+    $expect->extend('toBeQueuedEmailCount', function (int $count, ?string $transport = null): WebTestCase {
+        return runner(test(), 'assertQueuedEmailCount', [$count, $transport]);
     });
 
-    $expect->extend('toBeEmailIsQueued', function (MessageEvent $event): void {
-        $this->assertEmailIsQueued($event);
+    $expect->extend('toBeEmailIsQueued', function (MessageEvent $event): WebTestCase {
+        return runner(test(), 'assertEmailIsQueued', [$event]);
     });
 
-    $expect->extend('toBeEmailAttachmentCount', function (RawMessage $email, int $count): void {
-        $this->assertEmailAttachmentCount($email, $count);
+    $expect->extend('toBeEmailAttachmentCount', function (RawMessage $email, int $count): WebTestCase {
+        return runner(test(), 'assertEmailAttachmentCount', [$email, $count]);
     });
 
-    $expect->extend('toBeEmailTextBodyContains', function (RawMessage $email, string $text): void {
-        $this->assertEmailTextBodyContains($email, $text);
+    $expect->extend('toBeEmailTextBodyContains', function (RawMessage $email, string $text): WebTestCase {
+        return runner(test(), 'assertEmailTextBodyContains', [$email, $text]);
     });
 
-    $expect->extend('toBeEmailHtmlBodyContains', function (RawMessage $email, string $text): void {
-        $this->assertEmailHtmlBodyContains($email, $text);
+    $expect->extend('toBeEmailHtmlBodyContains', function (RawMessage $email, string $text): WebTestCase {
+        return runner(test(), 'assertEmailHtmlBodyContains', [$email, $text]);
     });
 
-    $expect->extend('toBeEmailHasHeader', function (RawMessage $email, string $headerName): void {
-        $this->assertEmailHasHeader($email, $headerName);
+    $expect->extend('toBeEmailHasHeader', function (RawMessage $email, string $headerName): WebTestCase {
+        return runner(test(), 'assertEmailHasHeader', [$email, $headerName]);
     });
 
-    $expect->extend('toBeEmailHeaderSame', function (RawMessage $email, string $headerName, string $expectedValue): void {
-        $this->assertEmailHeaderSame($email, $headerName, $expectedValue);
+    $expect->extend('toBeEmailHeaderSame', function (RawMessage $email, string $headerName, string $expectedValue): WebTestCase {
+        return runner(test(), 'assertEmailHeaderSame', [$email, $headerName, $expectedValue]);
     });
 
-    $expect->extend('toBeEmailAddressContains', function (RawMessage $email, string $headerName, string $expectedValue): void {
-        $this->assertEmailAddressContains($email, $headerName, $expectedValue);
+    $expect->extend('toBeEmailAddressContains', function (RawMessage $email, string $headerName, string $expectedValue): WebTestCase {
+        return runner(test(), 'assertEmailAddressContains', [$email, $headerName, $expectedValue]);
     });
 
-    $expect->extend('toBeEmailSubjectContains', function (RawMessage $email, string $expectedValue): void {
-        $this->assertEmailSubjectContains($email, $expectedValue);
+    $expect->extend('toBeEmailSubjectContains', function (RawMessage $email, string $expectedValue): WebTestCase {
+        return runner(test(), 'assertEmailSubjectContains', [$email, $expectedValue]);
     });
 }
